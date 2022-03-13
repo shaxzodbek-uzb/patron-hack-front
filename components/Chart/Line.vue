ecc1e81f3a27847bea6f413c2cfd16b1a10e4a3e
Shaxzodbek <shaxzodbek.qambaraliyev@gmail.com>
2022-03-13 12:10:12 +0500

    make default function

--- components/Chart/Line.vue
+++ components/Chart/Line.vue
@@ -5,8 +5,7 @@ export default {
   extends: Line,
   props: {
     chartdata: {
-      type: Object,
-      default: {
+      default: () => ({
         labels: [],
         datasets: [
           {
@@ -18,7 +17,7 @@ export default {
               'rgba(255, 206, 86, 0.2)',
               'rgba(75, 192, 192, 0.2)',
               'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+              'rgba(255, 159, 64, 0.2)',
             ],
             borderColor: [
               'rgba(255,99,132,1)',
@@ -26,16 +25,15 @@ export default {
               'rgba(255, 206, 86, 1)',
               'rgba(75, 192, 192, 1)',
               'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+              'rgba(255, 159, 64, 1)',
             ],
-            borderWidth: 1
-          }
-        ]
-      }
+            borderWidth: 1,
+          },
+        ],
+      }),
     },
     options: {
-      type: Object,
-      default: {
+      default: () => ({
         responsive: true,
         legend: {
           display: false,
@@ -60,17 +58,16 @@ export default {
           yAxes: [
             {
               ticks: {
-                beginAtZero: true
-              }
-            }
-          ]
-        }
-      }
-    }
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      }),
+    },
   },
-  mounted () {
+  mounted() {
     this.renderChart(this.chartdata, this.options)
-  }
+  },
 }
-
 </script>
