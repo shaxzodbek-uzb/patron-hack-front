e85f625ca1c7b7394b1d6a16719dfa8d71dd4b8b
Murod <murodjon.umarovv@gmail.com>
2022-03-14 20:29:38 +0500

    404 page

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid">
+    <!-- 404 Error Text -->
+    <div class="text-center margs">
+      <div class="error mx-auto" data-text="404">404</div>
+      <p class="lead text-gray-800 mb-5">Page Not Found</p>
+      <p class="text-gray-500 mb-0">
+        It looks like you found a glitch in the matrix...
+      </p>
+      <nuxt-link to="/"> 
+        &larr;  Back to Dashboard
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<style>
+    .margs {
+        margin-top: 10%;
+    }
+</style>
